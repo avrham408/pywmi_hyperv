30080235bdb16c4e599b9185cc2b4af7542a04ba
Avi Wertheimer <avrham408@gmail.com>
2021-06-27 01:49:21 +0300

    add more exceptions

--- pywmi_hyperv/exceptions.py
+++ pywmi_hyperv/exceptions.py
@@ -1,5 +1,18 @@
 class VmNotFound(Exception):
     pass
 
+
 class ConnectToWmiError(Exception):
+    pass
+
+
+class UnknownRc(Exception):
+    pass
+
+
+class JobNotExist(Exception):
+    pass
+
+
+class UnknownState(Exception):
     pass
